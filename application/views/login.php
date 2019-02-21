cb109165350e5bd204a697d9f5cae98a85bc86b0
Seppe <r0713024@student.thomasmore.be>
2019-02-21 15:00:31 +0100

    Knop registreer
    Knop registreren aangemaakt op de login pagina.

--- application/views/login.php
+++ application/views/login.php
@@ -1,1 +1,9 @@
 <h1>Welkom op de inlog pagina</h1>
+
+<input type="button" value="Registreer" href="registeer" />
+<div><a href='application/views/registreer.php'>Registreer</a></div>
+
+<?php
+
+
+?>
---
+++ application/views/registreer.php
@@ -0,0 +1,9 @@
+<h1>Huts</h1>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Seppe
+ * Date: 21-2-2019
+ * Time: 14:47
+ */
