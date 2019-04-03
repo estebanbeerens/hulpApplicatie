4302486b2ad135fb4bf1b348895ee7b04535f9f6
Tomas Marlein <tomas.marlein@gmail.com>
2019-04-03 13:34:18 +0200

    Layout event

--- application/views/evenementBeheren/evenementBeheren.php
+++ application/views/evenementBeheren/evenementBeheren.php
@@ -9,26 +9,7 @@
 
 </head>
 
-<style>
 
-    input[type=submit]{
-         background-color: #007BFF;
-         border: none;
-         padding: 7px 10px;
-         border-radius: 5px;
-         color: white;
-        cursor: pointer;
-    }
-
-    input[type=submit]:hover{
-        background-color: #005efe;
-    }
-
-    .anchorBewerken{
-        color: White;
-        text-decoration: none;
-    }
-</style>
 
 <body>
 <div id="eventbeheren">
@@ -64,79 +45,6 @@
             <?php } ?>
         </table>
     </div>
-
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Evenement updaten</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <?php
-                    $attributes = array('name' => 'evenementUpdatenFormulier');
-                    echo form_open('evenement/evenementUpdaten', $attributes);
-                    ?>
-                    <table>
-
-                        <tr>
-                            <td><?php echo form_label('Id:', 'id'); ?></td>
-                            <td><?php echo form_input(array('name' => 'id', 'id' => 'id', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Naam:', 'naam'); ?></td>
-                            <td><?php echo form_input(array('name' => 'naam', 'id' => 'naam', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Meldingtijd:', 'meldingtijd'); ?></td>
-                            <td><?php echo form_input(array('name' => 'meldingtijd', 'id' => 'meldingtijd', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Beschrijving:', 'beschrijving'); ?></td>
-                            <td><?php echo form_input(array('name' => 'beschrijving', 'id' => 'beschrijving', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Locatie:', 'locatie'); ?></td>
-                            <td><?php echo form_input(array('name' => 'locatie', 'id' => 'locatie', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Verplicht:', 'verplicht'); ?></td>
-                            <td><?php echo form_input(array('name' => 'verplicht', 'id' => 'verplicht', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Herhaling?', 'herhaling'); ?></td>
-                            <td><?php echo form_input(array('name' => 'herhaling', 'id' => 'herhaling', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Datum:', 'datum'); ?></td>
-                            <td><?php echo form_input(array('name' => 'datum', 'id' => 'datum', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Starttijd:', 'starttijd'); ?></td>
-                            <td><?php echo form_input(array('name' => 'starttijd', 'id' => 'starttijd', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-                        <tr>
-                            <td><?php echo form_label('Eindtijd:', 'eindtijd'); ?></td>
-                            <td><?php echo form_input(array('name' => 'eindtijd', 'id' => 'eindtijd', 'size' => '30', 'value' => '')); ?></td>
-                        </tr>
-
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <?php echo form_submit('knop', 'Save changes'); ?>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
-
 
 </div>
 
