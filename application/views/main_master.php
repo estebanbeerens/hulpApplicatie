e4828e072859a22e4baad87cd5a9d8f184a2e242
estebanbeerens <esteban.beerens@gmail.com>
2019-04-05 08:37:46 +0200

    Favicon toegevoegd

--- application/views/main_master.php
+++ application/views/main_master.php
@@ -10,6 +10,17 @@
         </title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
+        <!--Favicon-->
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/assets/favicon/site.webmanifest">
+        <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico">
+        <meta name="msapplication-TileColor" content="#003366">
+        <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml">
+        <meta name="theme-color" content="#003366">
+
         <?php echo pasStylesheetAan("bootstrap.css"); ?>
         <?php echo pasStylesheetAan("style.css"); ?>
         <?php echo pasStylesheetAan('menu.css') ?>
