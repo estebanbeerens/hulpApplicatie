8444cda55efe188ebecf9ddf803249825e8c9f5f
Jeroen246 <r0695294@student.thomasmore.be>
2019-05-02 14:33:45 +0200

    evenement tonen debuggen

--- application/views/laadEvenement.php
+++ application/views/laadEvenement.php
@@ -1,5 +1,10 @@
-<?php foreach($evenementen as $evenement){
-    if($evenement[0]->datum == date("Y-m-d")){
+<?php
+    $counter = 0;
+
+    foreach($evenementen as $evenement){
+
+    $counter++;
+    if(($counter < 4) && $evenement[0]->datum >= date("Y-m-d")){
         echo $evenement[0]->id . '<br>';
         echo $evenement[0]->naam . '<br>';
         echo $evenement[0]->datum . '<br>';
