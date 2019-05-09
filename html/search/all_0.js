100c34e518f1eff1987790897ba5ca013beba688
estebanbeerens <esteban.beerens@gmail.com>
2019-05-09 15:38:31 +0200

    Overbodige Doxygen rommel verwijderd.

--- html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['codeigniter',['CodeIgniter',['../namespace_code_igniter.html',1,'']]],
-  ['contributing_20to_20codeigniter',['Contributing to CodeIgniter',['../md_contributing.html',1,'']]]
-];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['contributing_20to_20codeigniter',['Contributing to CodeIgniter',['../md_contributing.html',1,'']]]
-];
